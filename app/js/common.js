f2b9ad50187edc4dece80e97c3f9514085fd55a0
Bohdan29 <gitarman@i.ua>
2017-12-08 17:55:41 +0200

    Added Prices block and Bloquotes block

--- app/js/common.js
+++ app/js/common.js
@@ -28,3 +28,35 @@ function showSlides(n) {
 
     slides[slideIndex-1].style.display = "block";
 };
+
+
+// buy now hover
+
+$(document).ready(function(){
+    $('.buyN1').hover(function() {
+        $(this).css({'backgroundColor' : '#475bf1'});
+        $('.titlePrice1').css({'backgroundColor' : '#475bf1'});
+    })
+    $('.buyN1').mouseout(function() {
+        $(this).css({'backgroundColor' : '#5b6ceb'});
+        $('.titlePrice1').css({'backgroundColor' : '#5b6ceb'});
+    })
+
+        $('.buyN2').hover(function() {
+        $(this).css({'backgroundColor' : '#475bf1'});
+        $('.titlePrice2').css({'backgroundColor' : '#475bf1'});
+    })
+    $('.buyN2').mouseout(function() {
+        $(this).css({'backgroundColor' : '#5b6ceb'});
+        $('.titlePrice2').css({'backgroundColor' : '#5b6ceb'});
+    })
+
+        $('.buyN3').hover(function() {
+        $(this).css({'backgroundColor' : '#475bf1'});
+        $('.titlePrice3').css({'backgroundColor' : '#475bf1'});
+    })
+    $('.buyN3').mouseout(function() {
+        $(this).css({'backgroundColor' : '#5b6ceb'});
+        $('.titlePrice3').css({'backgroundColor' : '#5b6ceb'});
+    })
+})
