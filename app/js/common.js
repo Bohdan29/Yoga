2033db0eb82b556cb41b7d06e27f81c9abba718f
Bohdan29 <gitarman@i.ua>
2017-12-09 16:50:30 +0200

    Added and fixed slider scripts in Reviews block and About us block. Added validation form script, fixed header script and slow anchor links script.Fixed markup in footer

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,68 @@
-// buy now hover
+//fixed header
+$(document).ready(function(){
+         
+        var $menu = $(".fixedHeader");
+             
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("default")
+                           .addClass("fixed transbg")
+                           .fadeIn('fast');
+                });
+            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("fixed transbg")
+                           .addClass("default")
+                           .fadeIn('fast');
+                });
+            }
+        });        
+});
 
+//slow anchor links 
+$(document).ready(function(){
+    var headerHeight = $('header').outerHeight();
+
+    $('.slideSection').click(function(e) {
+        var linkHref = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top - headerHeight
+        }, 1000);
+
+        e.preventDefault();
+    });
+});
+
+// validation form
+var field = new Array('name', 'phone');
+
+$('form').submit(function() {
+  var error = 0;
+  $('form').find('input').each(function() {
+    for (var i = 0; i < field.length; i++) {
+      if ($(this).attr('name') == field[i]) {
+        if (!$(this).val()) {
+          $(this).css('border', 'red 1px solid');
+          error = 1;
+        } else {
+          $(this).css('border', 'none');
+        }
+      }
+    }
+  });
+  if (error == 0) {
+    return true;
+  } else {
+    if (error == 1) var err_text = 'Not all fields are filled!';
+    $('.messenger').html(err_text);
+    $('.messenger').fadeIn('slow');
+    return false;
+  }
+});
+
+// "buy now" hover
 $(document).ready(function(){
     $('.buyN1').hover(function() {
         $(this).css({'backgroundColor' : '#475bf1'});
@@ -31,4 +94,17 @@ $(document).ready(function(){
 
 
 //slick slider 
-$('.single-item').slick();
+$('.single-item').slick({
+    dots: false
+});
+
+$('.review-slick').slick({
+    dots: true,
+    arrows: false
+});
+
+$('.trainer-slick').slick({
+    dots: false,
+    arrows: true
+});
+
